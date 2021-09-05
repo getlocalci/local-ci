8f68d466ddf3897853cddf853e780221888dc09a
Ryan Kienstra <kienstraryan@gmail.com>
2021-09-04 23:22:20 -0500

    Mock the library calls in getConfigFile()
    It'd be better to mock
    the function getConfigFile().

--- src/test/suite/utils/getCheckoutJobs.test.ts
+++ src/test/suite/utils/getCheckoutJobs.test.ts
@@ -1,18 +1,43 @@
 import * as assert from 'assert';
+import * as fs from 'fs';
 import * as mocha from 'mocha';
 import * as sinon from 'sinon';
+import * as yaml from 'js-yaml';
 import getCheckoutJobs from '../../../utils/getCheckoutJobs';
-import getConfigFile from '../../../utils/getConfigFile';
 
 mocha.afterEach(() => {
   sinon.restore();
 });
 
 suite('getCheckoutJobs', () => {
-  test('empty config file', () => {
-    sinon.spy(getConfigFile).returned({
-      jobs: { test: { docker: [{ image: 'cimg/node:16.8.0-browsers' }] } },
-    });
-    assert.strictEqual(getCheckoutJobs('foo'), []);
+  test('no checkout job', () => {
+    sinon.mock(fs).expects('readFileSync').once().returns('');
+    sinon
+      .mock(yaml)
+      .expects('load')
+      .once()
+      .returns({
+        jobs: { test: { docker: [{ image: 'cimg/node:16.8.0-browsers' }] } },
+      });
+
+    assert.notStrictEqual(getCheckoutJobs('foo'), []);
+  });
+
+  test('with string checkout job', () => {
+    sinon.mock(fs).expects('readFileSync').once().returns('');
+    sinon
+      .mock(yaml)
+      .expects('load')
+      .once()
+      .returns({
+        jobs: {
+          test: {
+            docker: [{ image: 'cimg/node:16.8.0-browsers' }],
+            steps: ['node/install-npm', 'checkout'],
+          },
+        },
+      });
+
+    assert.notStrictEqual(getCheckoutJobs('foo'), ['test']);
   });
 });
