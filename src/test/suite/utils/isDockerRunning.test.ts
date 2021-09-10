ca94daeaeb7383ac38c217e6eda9d2ee0ee06d85
Ryan Kienstra <kienstraryan@gmail.com>
2021-09-09 22:39:40 -0500

    Add a function to find if Docker is running
    If not, this will need a warning.

---
+++ src/test/suite/utils/isDockerRunning.test.ts
@@ -0,0 +1,25 @@
+import * as assert from 'assert';
+import * as cp from 'child_process';
+import * as mocha from 'mocha';
+import * as sinon from 'sinon';
+import isDockerRunning from '../../../utils/isDockerRunning';
+
+mocha.afterEach(() => {
+  sinon.restore();
+});
+
+suite('isDockerRunning', () => {
+  test('Is not running', () => {
+    sinon
+      .mock(cp)
+      .expects('spawnSync')
+      .once()
+      .returns({ stderror: 'Cannot connect to the Docker daemon' });
+    assert.strictEqual(isDockerRunning(), false);
+  });
+
+  test('Is running', () => {
+    sinon.mock(cp).expects('spawnSync').once().returns({ stderr: '' });
+    assert.strictEqual(isDockerRunning(), true);
+  });
+});
---
+++ src/utils/isDockerRunning.ts
@@ -0,0 +1,6 @@
+import * as cp from 'child_process';
+
+export default function isDockerRunning(): boolean {
+  const { stderr } = cp.spawnSync('docker', ['ps']);
+  return !stderr.length;
+}
