702523ceeb39a22d31f995dbe7fcbc2c6e17215d
Ryan Kienstra <kienstraryan@gmail.com>
2021-09-11 19:50:27 -0500

    Change /tmp directory back to /tmp
    According to CircleCI-Public,
    in macOS, the normal temp directories
    are not shared with Docker by default.
    https://github.com/CircleCI-Public/circleci-cli/blob/93b9d40689829c229f6aef55ba2d6c5c3d2ea498/local/local.go#L270-L275

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,3 @@
-import * as os from 'os';
-import * as path from 'path';
-
 export const RUN_JOB_COMMAND = 'local-ci.runJob';
-export const TMP_PATH = path.join(os.tmpdir(), 'local-ci');
-export const PROCESS_FILE_PATH = path.join(TMP_PATH, 'process.yml');
+export const TMP_PATH = '/tmp/local-ci';
+export const PROCESS_FILE_PATH = `${TMP_PATH}/process.yml`;
