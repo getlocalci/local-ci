55eb9f61db6d21cdc401933568dfde7b6fee4337
Ryan Kienstra <kienstraryan@gmail.com>
2021-09-15 19:56:29 -0500

    Attempt to fix failed unit test

--- src/test/suite/utils/cleanUpCommittedImage.test.ts
+++ src/test/suite/utils/cleanUpCommittedImage.test.ts
@@ -2,7 +2,6 @@ import * as cp from 'child_process';
 import * as mocha from 'mocha';
 import * as sinon from 'sinon';
 import cleanUpCommittedImage from '../../../utils/cleanUpCommittedImage';
-import getSpawnOptions from '../../../utils/getSpawnOptions';
 
 mocha.afterEach(() => {
   sinon.restore();
@@ -10,15 +9,8 @@ mocha.afterEach(() => {
 
 suite('cleanUpCommittedImage', () => {
   test('No error', () => {
-    const imageName = 'local-ci-lint';
-    sinon
-      .mock(cp)
-      .expects('spawnSync')
-      .once()
-      .withArgs('docker', ['rmi', imageName], getSpawnOptions());
+    sinon.mock(cp).expects('spawnSync').twice();
 
-    sinon.mock(cp).expects('spawnSync').once();
-
-    cleanUpCommittedImage(imageName);
+    cleanUpCommittedImage('local-ci-lint');
   });
 });
