aef552401994a69ddc9299491ab1a3500f681716
Ryan Kienstra <kienstraryan@gmail.com>
2021-09-19 16:32:12 -0500

    Revert a needless change to the pattern
    This was an attempt to fix a bug,
    but it didn't fix it.

--- src/utils/getImageWithoutTag.ts
+++ src/utils/getImageWithoutTag.ts
@@ -2,7 +2,7 @@
 // So this gets the image name without the tag.
 export default function getImageWithoutTag(image: string): string {
   const trimmedImage = image.trim();
-  const pattern = /([^\s]+):[^\s]+$/;
+  const pattern = /([^:]+):[^\s]+$/;
 
   return trimmedImage.match(pattern)
     ? (trimmedImage.match(pattern) as Array<string>)[1]
