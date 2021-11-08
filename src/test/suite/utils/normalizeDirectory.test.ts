605b911e9730d5a13154a6fb46c250ba75dadac8
Ryan Kienstra <kienstraryan@gmail.com>
2021-11-08 00:31:01 -0600

    Add test cases for normalizeDirectory.test.ts

--- src/test/suite/utils/normalizeDirectory.test.ts
+++ src/test/suite/utils/normalizeDirectory.test.ts
@@ -3,16 +3,29 @@ import normalizeDirectory from '../../../utils/normalizeDirectory';
 
 suite('convertHomeDirToAbsolute', () => {
   test('No home directory', () => {
-    assert.strictEqual(
-      normalizeDirectory('/foo/baz', '/root', undefined),
-      '/foo/baz'
-    );
+    assert.strictEqual(normalizeDirectory('/foo/baz', '/root', {}), '/foo/baz');
   });
 
   test('With home directory', () => {
     assert.strictEqual(
-      normalizeDirectory('~/foo/baz', '/home/project', undefined),
-      '/home/project/foo/baz'
+      normalizeDirectory('~/foo/baz', '/home/user', {}),
+      '/home/user/foo/baz'
+    );
+  });
+
+  test('With dot and no working_directory', () => {
+    assert.strictEqual(
+      normalizeDirectory('.', '/home/user', {}),
+      '/home/user/project'
+    );
+  });
+
+  test('With dot and working_directory', () => {
+    assert.strictEqual(
+      normalizeDirectory('.', '/home/user', {
+        working_directory: '/root/project', // eslint-disable-line @typescript-eslint/naming-convention
+      }),
+      '/root/project'
     );
   });
 });
