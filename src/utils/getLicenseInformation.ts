53ce8ade6acc278625887e37af084def79535ebf
Ryan Kienstra <kienstraryan@gmail.com>
2021-12-02 00:40:33 -0600

    Close the <p> tag, add a dynamic number of days

--- src/utils/getLicenseInformation.ts
+++ src/utils/getLicenseInformation.ts
@@ -11,6 +11,7 @@ import {
   LICENSE_VALIDITY,
   TRIAL_STARTED_TIMESTAMP,
   HAS_EXTENDED_TRIAL,
+  EXTENDED_TRIAL_LENGTH_IN_MILLISECONDS,
 } from '../constants';
 import getTimeRemainingInTrial from './getTimeRemainingInTrial';
 
@@ -21,12 +22,15 @@ export default async function getLicenseInformation(
   const previewStartedTimeStamp = context.globalState.get(
     TRIAL_STARTED_TIMESTAMP
   );
+  const daysInMilliseconds = 86400000;
   const licenseKey = await context.secrets.get(LICENSE_KEY);
   const getLicenseLink = `<a class="button secondary" href="${GET_LICENSE_KEY_URL}" target="_blank" rel="noopener noreferrer">Buy license</a>`;
   const enterLicenseButton = `<button class="secondary" id="enter-license">Enter license key</button>`;
   const changeLicenseButton = `<button class="secondary" id="enter-license">Change license key</button>`;
   const retryValidationButton = `<button class="secondary" id="retry-license-validation">Retry license validation</button>`;
-  const takeSurveyButton = `<a class="button primary" id="take-survey">Get 15 more free days by taking a 2-minute anonymous survey</a>`;
+  const takeSurveyButton = `<a class="button primary" id="take-survey">Get ${
+    EXTENDED_TRIAL_LENGTH_IN_MILLISECONDS / daysInMilliseconds
+  } more free days by taking a 2-minute anonymous survey</a>`;
 
   const isValid = await isLicenseValid(context);
   const hasExtendedTrial = !!context.globalState.get(HAS_EXTENDED_TRIAL);
@@ -47,7 +51,7 @@ export default async function getLicenseInformation(
       <p>This free trial will last for ${getPrettyPrintedTimeRemaining(
         trialLengthInMilliseconds
       )}.</p>
-      ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}<p>`}
+      ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}</p>`}
       <p>${getLicenseLink}</p>
       <p>${enterLicenseButton}</p>`;
   }
@@ -59,14 +63,14 @@ export default async function getLicenseInformation(
     )}</p>
     <p>${getLicenseLink}</p>
     <p>${enterLicenseButton}</p>
-    ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}<p>`}
+    ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}</p>`}
     <p>${retryValidationButton}</p>`;
   }
 
   if (isPreviewExpired) {
     return `<p>Thanks for previewing Local CI! The free preview is over.</p>
       <p>Please enter a Local CI license key to keep using this.</p>
-      ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}<p>`}
+      ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}</p>`}
       <p>${getLicenseLink}</p>
       <p>${enterLicenseButton}</p>`;
   }
@@ -76,8 +80,8 @@ export default async function getLicenseInformation(
       new Date().getTime(),
       context.globalState.get(TRIAL_STARTED_TIMESTAMP),
       trialLengthInMilliseconds
-    )} left in the free preview.</p>
-    ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}<p>`}
+    )} left in this free preview.</p>
+    ${hasExtendedTrial ? '' : `<p>${takeSurveyButton}</p>`}
     <p>${getLicenseLink}</p>
     <p>${enterLicenseButton}</p>`;
 }
