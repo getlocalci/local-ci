a6053dbfd8734637d565a47c491d9c62e32c8815
Ryan Kienstra <kienstraryan@gmail.com>
2021-12-02 00:16:38 -0600

    Prevent a JS error in addLicenseHandlers()
    Before, there was an error
    when the button wasn't in the DOM.

--- webview/index.js
+++ webview/index.js
@@ -3,24 +3,20 @@
 (function () {
   function addLicenseHandlers() {
     const vscode = acquireVsCodeApi();
+    const listenerElements = {
+      'take-survey': 'takeSurvey',
+      'enter-license': 'enterLicense',
+      'retry-license-validation': 'retryLicenseValidation',
+    };
 
-    document
-      .getElementById('take-survey')
-      .addEventListener('click', () =>
-        vscode.postMessage({ type: 'takeSurvey' })
-      );
-
-    document
-      .getElementById('enter-license')
-      .addEventListener('click', () =>
-        vscode.postMessage({ type: 'enterLicense' })
-      );
-
-    document
-      .getElementById('retry-license-validation')
-      .addEventListener('click', () =>
-        vscode.postMessage({ type: 'retryLicenseValidation' })
-      );
+    Object.keys(listenerElements).forEach((elementId) => {
+      const element = document.getElementById(elementId)
+      if (element) {
+        element.addEventListener('click', () =>
+          vscode.postMessage({ type: listenerElements[elementId] })
+        );
+      }
+    });
   }
 
   // Mainly copied from @wordpress/dom-ready https://github.com/WordPress/gutenberg/blob/3da717b8d0ac7d7821fc6d0475695ccf3ae2829f/packages/dom-ready/src/index.js#L31
