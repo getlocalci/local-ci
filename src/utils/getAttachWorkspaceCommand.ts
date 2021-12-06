680a13f829c30c6f72da7e85e2a95f4fc115f0f0
Ryan Kienstra <kienstraryan@gmail.com>
2021-12-06 15:10:21 -0600

    Check that the directory exists and that it's not empty before copying
    It looks like both checks are needed,
    though I might be missing a simpler solution.

--- src/utils/getAttachWorkspaceCommand.ts
+++ src/utils/getAttachWorkspaceCommand.ts
@@ -9,5 +9,6 @@ export default function getAttachWorkspaceCommand(step: Step): string {
   const attachFrom = path.join(CONTAINER_STORAGE_DIRECTORY, '*');
 
   // BusyBox doesn't have the -n option.
-  return `if [ -d ${CONTAINER_STORAGE_DIRECTORY} ]; then cp -rn ${attachFrom} ${step?.attach_workspace?.at} || cp -ru ${attachFrom} ${step?.attach_workspace?.at}; fi`;
+  // Check if the directory is empty before
+  return `if [[ -d ${CONTAINER_STORAGE_DIRECTORY} && ! -z "$(ls -A ${CONTAINER_STORAGE_DIRECTORY})" ]]; then cp -rn ${attachFrom} ${step?.attach_workspace?.at} || cp -ru ${attachFrom} ${step?.attach_workspace?.at}; fi`;
 }
