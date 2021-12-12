3376940049087ab20a0a797dd6decdb37216e1d2
Ryan Kienstra <kienstraryan@gmail.com>
2021-12-11 23:57:01 -0600

    Support multiple restore_cache keys, and cascading fallback
    If the 1st key doesn't match,
    it can fall back to another.
    https://circleci.com/docs/2.0/caching/#example-caching-configuration

--- src/test/suite/utils/getRestoreCacheCommand.test.ts
+++ src/test/suite/utils/getRestoreCacheCommand.test.ts
@@ -7,28 +7,94 @@ mocha.afterEach(() => {
   sinon.restore();
 });
 
+function normalize(text: string | undefined): string {
+  return String(text).replace(/\s+/g, ' ');
+}
+
 suite('getRestoreCacheCommand', () => {
-  test('Simple cache', () => {
+  test('simple key property', () => {
     assert.strictEqual(
-      getRestoreCacheCommand(
-        {
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          restore_cache: {
-            key: 'v2-deps-{{ checksum "package-lock.json" }}',
-          },
-        },
-        [
+      normalize(
+        getRestoreCacheCommand(
           {
-            paths: ['~/.npm', '~/.cache'],
-            key: 'v2-deps-{{ checksum "package-lock.json" }}',
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            restore_cache: {
+              key: 'v2-deps-{{ checksum "package-lock.json" }}',
+            },
           },
-          {
-            paths: ['node_modules'],
-            key: 'node-modules-{{ checksum "package-lock.json" }}',
-          },
-        ]
+          [
+            {
+              paths: ['~/.npm', '~/.cache'],
+              key: 'v2-deps-{{ checksum "package-lock.json" }}',
+            },
+            {
+              paths: ['node_modules'],
+              key: 'node-modules-{{ checksum "package-lock.json" }}',
+            },
+          ]
+        )
       ),
-      `if [ -d /tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )/.npm ]; then cp -rn /tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )/.npm ~ || cp -ru /tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )/.npm ~; fi \nif [ -d /tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )/.cache ]; then cp -rn /tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )/.cache ~ || cp -ru /tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )/.cache ~; fi \n`
+      normalize(
+        `restore_from_directories=("/tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )*/.npm");
+        for directory_candidate in $restore_from_directories; do
+          if [ $(ls -ard $directory_candidate 2>/dev/null) ]; then
+            verified_directory=$(ls -ard $directory_candidate | tail -n1)
+            cp -rn $verified_directory ~ || cp -ru $verified_directory ~;
+            break;
+          fi
+        done;
+        restore_from_directories=("/tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )*/.cache");
+        for directory_candidate in $restore_from_directories; do
+          if [ $(ls -ard $directory_candidate 2>/dev/null) ]; then
+            verified_directory=$(ls -ard $directory_candidate | tail -n1)
+            cp -rn $verified_directory ~ || cp -ru $verified_directory ~;
+            break;
+          fi
+        done;\n`
+      )
+    );
+  });
+
+  test('keys property with multiple keys', () => {
+    assert.strictEqual(
+      normalize(
+        getRestoreCacheCommand(
+          {
+            // eslint-disable-next-line @typescript-eslint/naming-convention
+            restore_cache: {
+              keys: ['v2-deps-{{ checksum "package-lock.json" }}', 'v2-deps'],
+            },
+          },
+          [
+            {
+              paths: ['~/.npm', '~/.cache'],
+              key: 'v2-deps-{{ checksum "package-lock.json" }}',
+            },
+            {
+              paths: ['node_modules'],
+              key: 'node-modules-{{ checksum "package-lock.json" }}',
+            },
+          ]
+        )
+      ),
+      normalize(
+        `restore_from_directories=("/tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )*/.npm" "/tmp/local-ci/v2-deps*/.npm");
+        for directory_candidate in $restore_from_directories; do
+          if [ $(ls -ard $directory_candidate 2>/dev/null) ]; then
+            verified_directory=$(ls -ard $directory_candidate | tail -n1)
+            cp -rn $verified_directory ~ || cp -ru $verified_directory ~;
+            break;
+          fi
+        done;
+        restore_from_directories=("/tmp/local-ci/v2-deps-$( shasum "package-lock.json" | awk '{print $1}' )*/.cache" "/tmp/local-ci/v2-deps*/.cache");
+        for directory_candidate in $restore_from_directories; do
+          if [ $(ls -ard $directory_candidate 2>/dev/null) ]; then
+            verified_directory=$(ls -ard $directory_candidate | tail -n1)
+            cp -rn $verified_directory ~ || cp -ru $verified_directory ~;
+            break;
+          fi
+        done;\n`
+      )
     );
   });
 });
--- src/utils/getRestoreCacheCommand.ts
+++ src/utils/getRestoreCacheCommand.ts
@@ -4,27 +4,43 @@ import { CONTAINER_STORAGE_DIRECTORY } from '../constants';
 
 // @todo: handle restore_cache having .keys.
 export default function getRestoreCacheCommand(
-  step: FullStep,
+  restoreCacheStep: FullStep,
   saveCacheSteps: (SaveCache | undefined)[]
-): string {
+): string | undefined {
   const originalSaveCacheStep = saveCacheSteps.find(
-    (saveCacheStep) => saveCacheStep?.key === step?.restore_cache?.key
+    (saveCacheStep) =>
+      saveCacheStep?.key === restoreCacheStep?.restore_cache?.key ||
+      restoreCacheStep?.restore_cache?.keys?.some(
+        (restoreCacheKey) => restoreCacheKey === saveCacheStep?.key
+      )
   );
 
   if (!originalSaveCacheStep) {
     return '';
   }
 
-  const cacheDirectory = path.join(
-    CONTAINER_STORAGE_DIRECTORY,
-    convertToBash(step?.restore_cache?.key ?? '')
-  );
-
-  return originalSaveCacheStep.paths.reduce((accumulator, saveCachePath) => {
-    const basename = path.basename(saveCachePath);
-    const fullCacheDirectory = path.join(cacheDirectory, basename);
+  return originalSaveCacheStep?.paths.reduce((accumulator, saveCachePath) => {
     const dirname = path.dirname(saveCachePath);
+    const restoreCacheKeys = restoreCacheStep?.restore_cache?.keys ?? [
+      restoreCacheStep?.restore_cache?.key ?? '',
+    ];
 
-    return `${accumulator}if [ -d ${fullCacheDirectory} ]; then cp -rn ${fullCacheDirectory} ${dirname} || cp -ru ${fullCacheDirectory} ${dirname}; fi \n`;
+    return `${accumulator}restore_from_directories=(${restoreCacheKeys
+      ?.map(
+        (restoreCacheKey) =>
+          `"${path.join(
+            CONTAINER_STORAGE_DIRECTORY,
+            convertToBash(restoreCacheKey) + '*', // To support cascading fallback: https://circleci.com/docs/2.0/caching/#example-caching-configuration
+            path.basename(saveCachePath)
+          )}"`
+      )
+      .join(' ')});
+    for directory_candidate in $restore_from_directories; do
+      if [ $(ls -ard $directory_candidate 2>/dev/null) ]; then
+        verified_directory=$(ls -ard $directory_candidate | tail -n1)
+        cp -rn $verified_directory ${dirname} || cp -ru $verified_directory ${dirname};
+        break;
+      fi
+    done;\n`;
   }, '');
 }
