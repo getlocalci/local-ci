813da14ad234d72dc29ed420adf7112d550a0684
Ryan Kienstra <kienstraryan@gmail.com>
2021-12-19 20:50:29 -0600

    Change Error: to Warning:
    Error might be a little too alarming,
    as it might be expected for it to happen.

--- src/test/suite/utils/getAttachWorkspaceCommand.test.ts
+++ src/test/suite/utils/getAttachWorkspaceCommand.test.ts
@@ -15,9 +15,9 @@ suite('getAttachWorkspaceCommand', () => {
       ),
       normalize(
         `if [ ! -d /tmp/local-ci ]; then
-          echo "Error: tried to attach_workspace to /tmp/local-ci, but it's not a directory. It might require a job to run before it."
+          echo "Warning: tried to attach_workspace to /tmp/local-ci, but it's not a directory. It might require a job to run before it."
         elif [ ! "$(ls -A /tmp/local-ci)" ]; then
-          echo "Error: tried to attach_workspace to /tmp/local-ci, but it's empty. It might require a job to run before it."
+          echo "Warning: tried to attach_workspace to /tmp/local-ci, but it's empty. It might require a job to run before it."
         else
           cp -rn /tmp/local-ci/. /foo/baz || cp -ru /tmp/local-ci/. /foo/baz
         fi`
--- src/utils/getAttachWorkspaceCommand.ts
+++ src/utils/getAttachWorkspaceCommand.ts
@@ -10,9 +10,9 @@ export default function getAttachWorkspaceCommand(step: Step): string {
 
   // BusyBox doesn't allow cp -n.
   return `if [ ! -d ${CONTAINER_STORAGE_DIRECTORY} ]; then
-      echo "Error: tried to attach_workspace to ${CONTAINER_STORAGE_DIRECTORY}, but it's not a directory. It might require a job to run before it."
+      echo "Warning: tried to attach_workspace to ${CONTAINER_STORAGE_DIRECTORY}, but it's not a directory. It might require a job to run before it."
     elif [ ! "$(ls -A ${CONTAINER_STORAGE_DIRECTORY})" ]; then
-      echo "Error: tried to attach_workspace to ${CONTAINER_STORAGE_DIRECTORY}, but it's empty. It might require a job to run before it."
+      echo "Warning: tried to attach_workspace to ${CONTAINER_STORAGE_DIRECTORY}, but it's empty. It might require a job to run before it."
     else
       cp -rn ${attachFrom} ${step?.attach_workspace?.at} || cp -ru ${attachFrom} ${step?.attach_workspace?.at}
     fi`;
