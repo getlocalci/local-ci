105add3566230ebcd6798c626ee0fc5005ed9e0b
Ryan Kienstra <kienstraryan@gmail.com>
2021-12-30 13:00:49 -0600

    Process the config differently so it can operate on the config first
    This will need to overwrite the continuation orb
    before circleci config process,
    so that it can store the resulting config
    file for later runs.
    The continuation orb doesn't work locally,
    not surprising, as it has a CIRCLE_CONTINUATION_KEY

--- src/utils/getProcessedConfig.ts
+++ src/utils/getProcessedConfig.ts
@@ -6,16 +6,14 @@ import getSpawnOptions from './getSpawnOptions';
 // The CircleCI CLI binary compiles that .yml file into another .yml file.
 // For example, it copies orbs into the file and evaluates the job parameters.
 export default function getProcessedConfig(configFilePath: string): string {
-  const { stdout, stderr } = cp.spawnSync(
-    getBinaryPath(),
-    ['config', 'process', configFilePath],
+  const { stdout, stderr } = cp.spawn(
+    '/bin/sh',
+    ['-c', `cat ${configFilePath} | ${getBinaryPath()} config process -`],
     getSpawnOptions()
   );
 
-  if (!stdout?.length) {
-    throw new Error(
-      stderr?.length ? stderr.toString() : 'Failed to process the config'
-    );
+  if (!stdout) {
+    throw new Error(stderr?.toString() ?? 'Failed to process the config');
   }
 
   return stdout.toString().trim();
