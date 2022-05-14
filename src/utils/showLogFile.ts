2397c49c21c2ab4c56bb145abd75832c007814b6
Ryan Kienstra <kienstraryan@gmail.com>
2022-05-13 21:27:19 -0500

    Remove a needless constant

--- src/utils/showLogFile.ts
+++ src/utils/showLogFile.ts
@@ -2,8 +2,9 @@ import * as vscode from 'vscode';
 import { LOG_SCHEME } from '../constants';
 
 export default async function showLogFile(logFilePath: string): Promise<void> {
-  const doc = await vscode.workspace.openTextDocument(
-    vscode.Uri.parse(`${LOG_SCHEME}:${logFilePath}`)
+  await vscode.window.showTextDocument(
+    await vscode.workspace.openTextDocument(
+      vscode.Uri.parse(`${LOG_SCHEME}:${logFilePath}`)
+    )
   );
-  await vscode.window.showTextDocument(doc, { preview: true });
 }
