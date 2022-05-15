36d42d6b384d5c91b8c3cd89928f8cc032b7545f
Ryan Kienstra <kienstraryan@gmail.com>
2022-05-15 14:03:31 -0500

    Move the empty string case to the top of the tests

--- src/test/suite/utils/getHash.test.ts
+++ src/test/suite/utils/getHash.test.ts
@@ -2,19 +2,19 @@ import * as assert from 'assert';
 import getHash from '../../../utils/getHash';
 
 suite('getHash', () => {
+  test('empty string', () => {
+    assert.strictEqual(
+      getHash(''),
+      'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+    );
+  });
+
   test('machineId format', () => {
     assert.strictEqual(
       getHash(
         'a923bcf3da6acff9c9bf4f129135ffd56adbfa294aeb8117c7264164c1a9382ca'
       ),
       'ea5320a945bfe117f3d9ee12268734180b697913fb7ff498549a5bb201255b09'
-    );
-  });
-
-  test('empty string', () => {
-    assert.strictEqual(
-      getHash(''),
-      'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
     );
   });
 });
--- src/utils/getHash.ts
+++ src/utils/getHash.ts
@@ -1,7 +1,7 @@
 import * as jsSHA from 'jssha';
 
 export default function getHash(toHash: string): string {
-  // @ts-ignore
+  // @ts-ignore The declaration file is wrong.
   const hash = new jsSHA('SHA-256', 'TEXT', { encoding: 'UTF8' });
   hash.update(toHash);
   return hash.getHash('HEX');
