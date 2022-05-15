cf1ba3e3153dcecf1e4f29f506ef93ab66706fec
Ryan Kienstra <kienstraryan@gmail.com>
2022-05-15 14:06:17 -0500

    Add another test case for getHash()

--- src/test/suite/utils/getHash.test.ts
+++ src/test/suite/utils/getHash.test.ts
@@ -9,12 +9,21 @@ suite('getHash', () => {
     );
   });
 
-  test('machineId format', () => {
+  test('possible machineId 1', () => {
     assert.strictEqual(
       getHash(
         'a923bcf3da6acff9c9bf4f129135ffd56adbfa294aeb8117c7264164c1a9382ca'
       ),
       'ea5320a945bfe117f3d9ee12268734180b697913fb7ff498549a5bb201255b09'
+    );
+  });
+
+  test('possible machineId 2', () => {
+    assert.strictEqual(
+      getHash(
+        '52ab60b09f4e8fae2bcb8ac38871cfa8613ee4a1d566dcec5659e320148dc366e'
+      ),
+      'f600f144c3c96701f3c34ddd9fb65089af27fb8264fcba12f40af8d6cbbddeeb'
     );
   });
 });
