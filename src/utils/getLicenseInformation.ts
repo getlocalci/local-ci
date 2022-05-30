7a3a49809d9c84e3a1000dc4d53e57bb4411e61b
Ryan Kienstra <kienstraryan@gmail.com>
2022-05-29 19:39:22 -0500

    Add 15 more days to the bump for taking a survey

--- src/utils/getLicenseInformation.ts
+++ src/utils/getLicenseInformation.ts
@@ -12,6 +12,7 @@ import {
   LICENSE_KEY,
   LICENSE_VALIDITY,
   SCHEDULE_INTERVIEW_URL,
+  TRIAL_LENGTH_IN_MILLISECONDS,
   TRIAL_STARTED_TIMESTAMP,
 } from '../constants';
 
@@ -29,8 +30,9 @@ export default async function getLicenseInformation(
   const changeLicenseButton = `<button class="secondary" id="enter-license">Change license key</button>`;
   const retryValidationButton = `<button class="secondary" id="retry-license-validation">Retry license validation</button>`;
   const takeSurveyButton = `<button class="button primary" id="take-survey">Get ${
-    EXTENDED_TRIAL_LENGTH_IN_MILLISECONDS / dayInMilliseconds
-  } more free days by taking a 2-minute anonymous survey</button>`;
+    (TRIAL_LENGTH_IN_MILLISECONDS + EXTENDED_TRIAL_LENGTH_IN_MILLISECONDS) /
+    dayInMilliseconds
+  } more free days by taking a 2-minute survey</button>`;
   const scheduleInterviewLink = `<a class="button primary" href="${SCHEDULE_INTERVIEW_URL}" target="_blank">Get a free lifetime license by doing a 30-minute Zoom user research interview</a>`;
   const complainUri = 'mailto:ryan@getlocalci.com';
   const complainLink = `<a class="button secondary" href="${complainUri}" target="_blank">Complain to me</a>`;
