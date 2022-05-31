5cbc284fbeae9d828382bd6818d3d117133cd2db
Ryan Kienstra <kienstraryan@gmail.com>
2022-05-30 20:57:26 -0500

    Add a button to get started after entering email

--- src/extension.ts
+++ src/extension.ts
@@ -56,7 +56,7 @@ export function activate(context: vscode.ExtensionContext): void {
   if (!context.globalState.get(TRIAL_STARTED_TIMESTAMP)) {
     context.globalState.update(TRIAL_STARTED_TIMESTAMP, new Date().getTime());
     reporter.sendTelemetryEvent('firstActivation');
-    askForEmail();
+    askForEmail(reporter);
 
     const getStartedText = 'Get started debugging faster';
     vscode.window
--- src/utils/askForEmail.ts
+++ src/utils/askForEmail.ts
@@ -1,43 +1,48 @@
 import * as vscode from 'vscode';
+import TelemetryReporter from '@vscode/extension-telemetry';
 import sendEnteredEmail from './sendEnteredEmail';
 
-export default function askForEmail(): void {
-  vscode.window
-    .showInputBox({
-      title: 'Email',
-      prompt:
-        'Could you please enter your email for your free preview of Local CI?',
-    })
-    .then((enteredEmail) => {
-      if (enteredEmail === undefined) {
-        return; // They pressed Escape or exited the input box.
-      }
+export default async function askForEmail(
+  reporter: TelemetryReporter
+): Promise<void> {
+  const enteredEmail = await vscode.window.showInputBox({
+    title: 'Email',
+    prompt:
+      'Could you please enter your email for your free preview of Local CI?',
+  });
 
-      vscode.window
-        .showInputBox({
-          title: 'Name',
-          prompt: `Thanks a lot! What's your first name?`,
-        })
-        .then((enteredName) => {
-          if (enteredName === undefined) {
-            sendEnteredEmail(enteredEmail); // They pressed Escape or exited the input box.
-            return;
-          }
+  if (enteredEmail === undefined) {
+    return; // They pressed Escape or exited the input box.
+  }
 
-          const yesText = 'Yes';
-          vscode.window
-            .showQuickPick(['No', yesText], {
-              title: `Thanks so much! Can I send you occasional emails with CI/CD tips? You can unsubscribe any time. I'll never share your email.`,
-            })
-            .then(async (selection) => {
-              const optedIntoNewsletter = selection === yesText;
+  const enteredName = await vscode.window.showInputBox({
+    title: 'Name',
+    prompt: `Thanks a lot! What's your first name?`,
+  });
+  if (enteredName === undefined) {
+    sendEnteredEmail(enteredEmail); // They pressed Escape or exited the input box.
+    return;
+  }
 
-              vscode.window.showInformationMessage(
-                'Thanks a lot, your free preview has started!',
-                { detail: 'Local CI free preview' }
-              );
-              sendEnteredEmail(enteredEmail, enteredName, optedIntoNewsletter);
-            });
-        });
-    });
+  const yesText = 'Yes';
+  const optInSelection = await vscode.window.showQuickPick(['No', yesText], {
+    title: `Thanks so much! Can I send you occasional emails with CI/CD tips? You can unsubscribe any time. I'll never share your email.`,
+  });
+  const optedIntoNewsletter = optInSelection === yesText;
+  sendEnteredEmail(enteredEmail, enteredName, optedIntoNewsletter);
+
+  const getStartedText = 'Get started';
+  const buttonClicked = await vscode.window.showInformationMessage(
+    'Thanks a lot, your free preview has started!',
+    { detail: 'Local CI free preview' },
+    getStartedText
+  );
+
+  if (buttonClicked === getStartedText) {
+    vscode.commands.executeCommand(
+      'workbench.action.openWalkthrough',
+      'LocalCI.local-ci#welcomeLocalCi'
+    );
+    reporter.sendTelemetryEvent('click.getStarted');
+  }
 }
