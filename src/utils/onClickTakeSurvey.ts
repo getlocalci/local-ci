90aef97cc1033d9f2c78fd303a290b3ae677de0a
Ryan Kienstra <kienstraryan@gmail.com>
2022-06-19 15:53:35 -0500

    Add a handler for clicking to take the survey

---
+++ src/utils/onClickTakeSurvey.ts
@@ -0,0 +1,32 @@
+import * as vscode from 'vscode';
+import {
+  EXTENDED_TRIAL_LENGTH_IN_MILLISECONDS,
+  HAS_EXTENDED_TRIAL,
+  SURVEY_URL,
+  TRIAL_LENGTH_IN_MILLISECONDS,
+  TRIAL_STARTED_TIMESTAMP,
+} from '../constants';
+import getPrettyPrintedTimeRemaining from './getPrettyPrintedTimeRemaining';
+
+export default async function onClickTakeSurvey(
+  context: vscode.ExtensionContext,
+  successCallback: function
+) {
+  if (context.globalState.get(HAS_EXTENDED_TRIAL)) {
+    return;
+  }
+
+  await context.globalState.update(HAS_EXTENDED_TRIAL, true);
+  await context.globalState.update(
+    TRIAL_STARTED_TIMESTAMP,
+    new Date().getTime()
+  );
+  successCallback();
+
+  vscode.env.openExternal(vscode.Uri.parse(SURVEY_URL));
+  vscode.window.showInformationMessage(
+    `Thanks, your free preview is now ${getPrettyPrintedTimeRemaining(
+      TRIAL_LENGTH_IN_MILLISECONDS + EXTENDED_TRIAL_LENGTH_IN_MILLISECONDS
+    )} longer`
+  );
+}
