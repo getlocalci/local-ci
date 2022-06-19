3a94759c9893bd7ba210da1f2ab0ad33ab093969
Ryan Kienstra <kienstraryan@gmail.com>
2022-06-19 18:48:39 -0500

    Remove the needless button property

--- src/classes/IconCommand.ts
+++ src/classes/IconCommand.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode';
 import Command from './Command';
 
 export default class IconCommand extends Command {
-  button?: vscode.TreeItem;
-
   constructor(public readonly label: string, command: string, icon: string) {
     super(label, command);
     this.iconPath = new vscode.ThemeIcon(icon);
