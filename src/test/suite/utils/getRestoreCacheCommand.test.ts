27854b152384ddcc5fd404222e9a6a64d83fd477
Ryan Kienstra <kienstraryan@gmail.com>
2022-08-07 18:37:25 -0500

    Wrap find statement in double quotes

--- src/test/suite/utils/getRestoreCacheCommand.test.ts
+++ src/test/suite/utils/getRestoreCacheCommand.test.ts
@@ -34,7 +34,7 @@ suite('getRestoreCacheCommand', () => {
         `restore_from_directories=("/tmp/local-ci/v2-deps-$( if [ -f "package-lock.json" ]; then (shasum "package-lock.json" 2>/dev/null || sha256sum "package-lock.json" 2>/dev/null) | awk '{print $1}'; fi )*/.npm")
         for directory_candidate in $restore_from_directories
           do
-          if [ $(find $directory_candidate 2>/dev/null) ]
+          if [ "$(find $directory_candidate 2>/dev/null)" ]
             then
             verified_directory=$(find $directory_candidate | tail -n1) echo "Restoring cached directory $verified_directory";
             cp -rn $verified_directory ~ || cp -ru $verified_directory ~;
@@ -44,7 +44,7 @@ suite('getRestoreCacheCommand', () => {
         restore_from_directories=("/tmp/local-ci/v2-deps-$( if [ -f "package-lock.json" ]; then (shasum "package-lock.json" 2>/dev/null || sha256sum "package-lock.json" 2>/dev/null) | awk '{print $1}'; fi )*/.cache")
         for directory_candidate in $restore_from_directories
           do
-          if [ $(find $directory_candidate 2>/dev/null) ]
+          if [ "$(find $directory_candidate 2>/dev/null)" ]
             then
             verified_directory=$(find $directory_candidate | tail -n1)
             echo "Restoring cached directory $verified_directory";
@@ -81,7 +81,7 @@ suite('getRestoreCacheCommand', () => {
         `restore_from_directories=("/tmp/local-ci/v2-deps-$( if [ -f "package-lock.json" ]; then (shasum "package-lock.json" 2>/dev/null || sha256sum "package-lock.json" 2>/dev/null) | awk '{print $1}'; fi )*/.npm" "/tmp/local-ci/v2-deps*/.npm")
         for directory_candidate in $restore_from_directories
           do
-          if [ $(find $directory_candidate 2>/dev/null) ]
+          if [ "$(find $directory_candidate 2>/dev/null)" ]
             then
             verified_directory=$(find $directory_candidate | tail -n1)
             echo "Restoring cached directory $verified_directory";
@@ -92,7 +92,7 @@ suite('getRestoreCacheCommand', () => {
         restore_from_directories=("/tmp/local-ci/v2-deps-$( if [ -f "package-lock.json" ]; then (shasum "package-lock.json" 2>/dev/null || sha256sum "package-lock.json" 2>/dev/null) | awk '{print $1}'; fi )*/.cache" "/tmp/local-ci/v2-deps*/.cache")
         for directory_candidate in $restore_from_directories
           do
-          if [ $(find $directory_candidate 2>/dev/null) ]
+          if [ "$(find $directory_candidate 2>/dev/null)" ]
           then
             verified_directory=$(find $directory_candidate | tail -n1)
             echo "Restoring cached directory $verified_directory";
