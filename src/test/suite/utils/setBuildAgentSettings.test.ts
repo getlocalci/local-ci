246f9cd7b926bd0c53fc807e8fd8a38eb43bf228
Ryan Kienstra <kienstraryan@gmail.com>
2022-08-27 19:44:44 -0500

    Correct a failed unit test, add another test for M1 Mac

--- src/test/suite/utils/setBuildAgentSettings.test.ts
+++ src/test/suite/utils/setBuildAgentSettings.test.ts
@@ -10,7 +10,7 @@ mocha.afterEach(() => {
 });
 
 suite('setBuildAgentSettings', () => {
-  test('should set the settings on a Linux machine', () => {
+  test('should not set the settings on a Linux machine', () => {
     const spawnSpy = sinon.spy();
     sinon.stub(os, 'type').value(() => 'Intel');
     sinon.stub(os, 'arch').value(() => 'x64');
@@ -20,9 +20,19 @@ suite('setBuildAgentSettings', () => {
     assert.equal(spawnSpy.called, false);
   });
 
+  test('should not set the settings on an M1 Mac machine', () => {
+    const spawnSpy = sinon.spy();
+    sinon.stub(os, 'type').value(() => 'Darwin');
+    sinon.stub(os, 'arch').value(() => 'arm64');
+    sinon.stub(cp, 'spawn').value(spawnSpy);
+
+    setBuildAgentSettings();
+    assert.equal(spawnSpy.called, false);
+  });
+
   test('should set the settings on an Intel Mac machine', () => {
     const spawnSpy = sinon.spy();
-    sinon.stub(os, 'type').value(() => 'Intel');
+    sinon.stub(os, 'type').value(() => 'Darwin');
     sinon.stub(os, 'arch').value(() => 'x64');
     sinon.stub(cp, 'spawn').value(spawnSpy);
 
