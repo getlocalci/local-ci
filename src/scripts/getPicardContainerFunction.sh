fbffa9c74f20f90e450a06607f1b6997590ea5b1
Ryan Kienstra <kienstraryan@gmail.com>
2022-08-27 23:47:05 -0500

    Fix the shellcheck errors in get_picard_container()

--- src/scripts/getPicardContainerFunction.sh
+++ src/scripts/getPicardContainerFunction.sh
@@ -4,9 +4,9 @@ get_picard_container() {
   JOB_NAME=$1
   for container in $(docker ps -q);
     do
-    if [ "$(docker inspect $(docker inspect $container --format {{.Image}}) --format {{.RepoDigests}}) == *"circleci/picard"* ] && [ $(docker inspect $container --format {{.Args}} | grep $JOB_NAME)" ]
+    if docker inspect "$container" --format '{{.Args}}' | grep -q "$JOB_NAME" && docker inspect "$(docker inspect "$container" --format '{{.Image}}')" --format '{{.RepoDigests}}' | grep -q "circleci/picard"
       then
-      echo $container
+      echo "$container"
       break
     fi
     done
