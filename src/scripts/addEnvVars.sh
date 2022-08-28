f52c1f085ec00d8629a476c49c90dbc60a52b115
Ryan Kienstra <kienstraryan@gmail.com>
2022-08-28 00:23:09 -0500

    Fix shellcheck errors in addEnvVars

---
+++ src/scripts/addEnvVars.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# shellcheck disable=SC2016
+
+{
+echo 'export CIRCLE_SHA1=$(git rev-parse HEAD)'
+echo 'export CIRCLE_BRANCH=$(git rev-parse --abbrev-ref HEAD)'
+echo 'export CIRCLE_PROJECT_REPONAME=$(basename $(git remote get-url origin))'
+} >> "$BASH_ENV"
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,4 @@
+import scriptAddEnvVars from './addEnvVars.sh';
 import scriptGetPicardContainerFunction from './getPicardContainerFunction.sh';
 import scriptGetRunningContainerFunction from './getRunningContainerFunction.sh';
 
@@ -5,6 +6,7 @@ function stripBinBash(file: string) {
   return file.replace(`#!/bin/sh\n\n`, '');
 }
 
+export const addEnvVars = stripBinBash(scriptAddEnvVars);
 export const getPicardContainerFunction = stripBinBash(
   scriptGetPicardContainerFunction
 );
