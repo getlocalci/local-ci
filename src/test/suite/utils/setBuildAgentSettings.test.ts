3baac7aadfe452272f8692f6fb069c0aece584e4
Ryan Kienstra <kienstraryan@gmail.com>
2022-08-27 19:39:16 -0500

    Prevent an error on Intel Macs
    Though the latest version of the CCI binary
    doesn't write a build_agent_settings.json,
    that binary doesn't work on Intel Mac.
    So write that .json file
    with the agent version that will work
    on an Intel Mac.
    The latest agent doesn't work.

---
+++ src/test/suite/utils/setBuildAgentSettings.test.ts
@@ -0,0 +1,32 @@
+import * as assert from 'assert';
+import * as cp from 'child_process';
+import * as mocha from 'mocha';
+import * as os from 'os';
+import * as sinon from 'sinon';
+import setBuildAgentSettings from '../../../utils/setBuildAgentSettings';
+
+mocha.afterEach(() => {
+  sinon.restore();
+});
+
+suite('setBuildAgentSettings', () => {
+  test('should set the settings on a Linux machine', () => {
+    const spawnSpy = sinon.spy();
+    sinon.stub(os, 'type').value(() => 'Intel');
+    sinon.stub(os, 'arch').value(() => 'x64');
+    sinon.stub(cp, 'spawn').value(spawnSpy);
+
+    setBuildAgentSettings();
+    assert.equal(spawnSpy.called, false);
+  });
+
+  test('should set the settings on an Intel Mac machine', () => {
+    const spawnSpy = sinon.spy();
+    sinon.stub(os, 'type').value(() => 'Intel');
+    sinon.stub(os, 'arch').value(() => 'x64');
+    sinon.stub(cp, 'spawn').value(spawnSpy);
+
+    setBuildAgentSettings();
+    assert.equal(spawnSpy.called, true);
+  });
+});
