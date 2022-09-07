c84e0c5244522afec484ac2caa17521803a75a05
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-06 19:02:29 -0500

    Move getDebuggingTerminalName.test.ts to terminal/

---
+++ src/terminal/test/getDebuggingTerminalName.test.ts
@@ -0,0 +1,7 @@
+import getDebuggingTerminalName from '../getDebuggingTerminalName';
+
+test('getDebuggingTerminalName', () => {
+  expect(getDebuggingTerminalName('build')).toEqual('Local CI debugging build');
+
+  expect(getDebuggingTerminalName('')).toEqual('Local CI debugging ');
+});
