36707afa9f597c38ac14cb90d1fc7193cc1585cd
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-06 19:04:33 -0500

    Move getTerminalName.test.ts to terminal/

--- src/terminal/test/getFinalTerminalName.test.ts
+++ src/terminal/test/getFinalTerminalName.test.ts
@@ -1,10 +1,8 @@
 import getFinalTerminalName from 'terminal/getFinalTerminalName';
 
 describe('getFinalTerminalName', () => {
-  test('errors', () => {
-    expect(getFinalTerminalName('composer')).toEqual(
-      'Local CI final debugging composer'
-    );
-    expect(getFinalTerminalName('')).toEqual('Local CI final debugging ');
-  });
+  expect(getFinalTerminalName('composer')).toEqual(
+    'Local CI final debugging composer'
+  );
+  expect(getFinalTerminalName('')).toEqual('Local CI final debugging ');
 });
--- src/test-tools/suite/utils/getTerminalName.test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from 'assert';
-import getTerminalName from 'terminal/getTerminalName';
-
-suite('getTerminalName', () => {
-  test('errors', () => {
-    assert.strictEqual(getTerminalName('test'), 'Local CI test');
-    assert.strictEqual(getTerminalName(''), 'Local CI ');
-  });
-});
