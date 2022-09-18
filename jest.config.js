664e547f6bfec2400ea0aaf33b2de4214bfeb6a6
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-18 13:35:20 -0500

    Update jest.config.js with the new directory

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleDirectories: ['node_modules', 'src'],
-  setupFilesAfterEnv: ['<rootDir>/src/test-tools/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test-tool/jest.setup.ts'],
   transform: {
-    '\\.sh$': '<rootDir>/src/test-tools/jest-raw-loader.js',
-    '\\.yml$': '<rootDir>/src/test-tools/jest-raw-loader.js'
+    '\\.sh$': '<rootDir>/src/test-tool/jest-raw-loader.js',
+    '\\.yml$': '<rootDir>/src/test-tool/jest-raw-loader.js'
   }
 };
