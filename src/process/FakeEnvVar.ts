5aad98f9563ce828b805ae03fef6567801e15239
Ryan Kienstra <kienstraryan@gmail.com>
2022-10-17 00:45:12 -0500

    Update the unit test expected .yml files

--- src/process/FakeEnvVar.ts
+++ src/process/FakeEnvVar.ts
@@ -6,10 +6,10 @@ export default class FakeEnvVar {
     return {
       run: {
         name: 'Set more environment variables',
-        command: `echo "export CIRCLE_SHA1=037f2d35d59080dc313608c565b55f37d2f42c7e"
+        command: `echo 'echo "export CIRCLE_SHA1=037f2d35d59080dc313608c565b55f37d2f42c7e"
         echo "export CIRCLE_BRANCH=develop"
         echo "export CIRCLE_PROJECT_REPONAME=local-ci"
-        echo "export CIRCLE_REPOSITORY_URL=git@github.com:getlocalci/local-ci.git"`,
+        echo "export CIRCLE_REPOSITORY_URL=git@github.com:getlocalci/local-ci.git"' >> $BASH_ENV`,
       },
     };
   }
