d1c502667d1e236bfa708d1150c4ed0a57e7fafa
Ryan Kienstra <kienstraryan@gmail.com>
2022-10-18 01:37:39 -0500

    Add the test I forgot to stage

---
+++ src/config/test/getAttachWorkspaceCommand.spec.ts
@@ -0,0 +1,29 @@
+import getAttachWorkspaceCommand from 'config/getAttachWorkspaceCommand';
+import normalize from 'test-tool/helper/normalize';
+
+describe('getAttachWorkspaceCommand', () => {
+  test('no attach_workspace', () => {
+    expect(getAttachWorkspaceCommand({})).toEqual('');
+  });
+
+  test('with attach_workspace', () => {
+    expect(
+      normalize(
+        getAttachWorkspaceCommand({ attach_workspace: { at: '/foo/baz' } })
+      )
+    ).toEqual(
+      normalize(
+        `if [ ! -d /tmp/local-ci ]
+          then
+          echo "Warning: tried to attach_workspace to /tmp/local-ci, but it's not a directory. It might require a job to run before it."
+        elif [ ! "$(ls -A /tmp/local-ci)" ]
+          then
+          echo "Warning: tried to attach_workspace to /tmp/local-ci, but it's empty. It might require a job to run before it."
+        else
+          echo "Attaching /tmp/local-ci/."
+          cp -Lr /tmp/local-ci/. /foo/baz
+        fi`
+      )
+    );
+  });
+});
